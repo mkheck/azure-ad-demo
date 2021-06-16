47675bf916de6bdbdfd84063cf2de8c5f46053c4
Mark Heckler <markheckler@jarvis.local>
2021-06-15 21:00:19 -0500

    Initial commit

---
+++ src/test/java/com/thehecklers/azureaddemo/AzureAdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thehecklers.azureaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureAdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
